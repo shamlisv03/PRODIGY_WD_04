860805e4b4f1b0d01c0b13a60a3c289f1b2c73d8
shamlisv03 <shamlisv03@gmail.com>
2024-02-12 21:29:12 +0530

    initial

---
+++ task4.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Update the current year in the footer
+document.querySelector('footer p').textContent += new Date().getFullYear();
